5bbd854113b6e897783eb8540fee40d8d7472fed
cody-papcol <cody.aitken@papamoacollege.school.nz>
2026-09-13 13:54:44 +1200

    Adding pause functionality

---
+++ waterblast-2026-08-07-13-14-31/scripts/pause_controller.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+@onready var player = $".."
+
+func _process(delta):
+	if Input.is_action_just_pressed("escape"):
+		get_tree().paused = not get_tree().paused
+		
+		if get_tree().paused == true:
+			
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			$"../HUD/Control/PauseBackground".visible = true
+			
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			$"../HUD/Control/PauseBackground".visible = false
+		
+		# fixing player sprint animation issues
+		if get_tree().paused == true and player.sprinting == true:
+			$"../AnimationPlayer".play_backwards("sprint")
+
+
+func _on_resume_button_pressed():
+	
+	get_tree().paused = false
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	$"../HUD/Control/PauseBackground".visible = false
+
+
+func _on_quit_button_pressed():
+	get_tree().paused = false
+	
+	get_tree().change_scene_to_file("res://levels/main_menu.tscn")
--- waterblast-2026-08-07-13-14-31/scripts/player.gd
+++ waterblast-2026-08-07-13-14-31/scripts/player.gd
@@ -23,6 +23,7 @@ var bulletDamage = 0
 
 var unlockedWeapons = 0
 var level = 0
+var playerPaused = false
 
 # individual weapon variables
 # pistol
@@ -235,217 +236,212 @@ func change_weapon(num):
 	ammo = 0
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * SENSITIVITY)
-		camera.rotate_x(-event.relative.y * SENSITIVITY)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-85), deg_to_rad(85))
+	
 		
-	# escape button to menu
-	if Input.is_action_just_pressed("escape"):
+	# escape button to pause
+	if Input.is_action_just_pressed("interact"):
 		if in_shop:
 			close_shop()
 			in_shop = false
-		else:
-			get_tree().change_scene_to_file("res://levels/main_menu.tscn")
+			
+			
+	# input only works when gameplay is not paused
 	
+	if in_shop == false:
+		# mouse camera movement
+		if event is InputEventMouseMotion:
+			head.rotate_y(-event.relative.x * SENSITIVITY)
+			camera.rotate_x(-event.relative.y * SENSITIVITY)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-85), deg_to_rad(85))
 		
-	# initial footstep sounds
-	if Input.is_action_just_pressed("forward") or Input.is_action_just_pressed("back") or Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right"):
-		if $FootstepTimer.is_stopped():
-			$FootstepTimer.start()
-	
-	# sprint animation
-	if Input.is_action_just_pressed("sprint") and reloading == false:
-		$AnimationPlayer.play("sprint")
-		sprinting = true
-		$FootstepTimer.wait_time = sprinting_wait_time
+		# initial footstep sounds
+		if Input.is_action_just_pressed("forward") or Input.is_action_just_pressed("back") or Input.is_action_just_pressed("left") or Input.is_action_just_pressed("right"):
+			if $FootstepTimer.is_stopped():
+				$FootstepTimer.start()
 		
-	if Input.is_action_just_pressed("interact"):
-		activate()
-	
-	#handling reload
-	if Input.is_action_just_pressed("reload") and ammo != max_ammo and total_water != 0:
+		# sprint animation
+		if Input.is_action_just_pressed("sprint") and reloading == false:
+			$AnimationPlayer.play("sprint")
+			sprinting = true
+			$FootstepTimer.wait_time = sprinting_wait_time
+			
+		if Input.is_action_just_pressed("interact"):
+			activate()
 		
-		reload()
+		#handling reload
+		if Input.is_action_just_pressed("reload") and ammo != max_ammo and total_water != 0:
+			
+			reload()
 	
-	
-	# handling weapon select
-	#if not in_shop:
-		#if Input.is_action_just_pressed("WeaponSlot1"):
-		#	change_weapon(0)
-	#	if Input.is_action_just_pressed("WeaponSlot2"):
-		#	change_weapon(1)
-		#if Input.is_action_just_pressed("WeaponSlot3"):
-		#	change_weapon(2)
-		#if Input.is_action_just_pressed("WeaponSlot4"):
-		#	change_weapon(3)
 	
 	
 func _physics_process(delta: float) -> void:
 	
-	# shooting mechanics, USE JUST PRESSED NOT PRESSED
-	if Input.is_action_pressed("shoot") and reloading == false and sprinting == false:
-		
-		# checking with delay
-		if can_shoot and not in_shop:
+	# only processes if not paused
+	if playerPaused == false:
+		# shooting mechanics
+		if Input.is_action_pressed("shoot") and reloading == false and sprinting == false:
 			
-			if !blocking.is_colliding():
+			# checking with delay
+			if can_shoot and not in_shop:
 				
-				# dealing with ammo
-				if ammo > 0:
+				if !blocking.is_colliding():
 					
-					
-					if weapon == 2:
+					# dealing with ammo
+					if ammo > 0:
 						
-						#shotgun bullet spawns
-						if ammo >= shotgunPellets:
-							for x in shotgunPellets:
-								var new_bullet : RigidBody3D = bullet_prefab.instantiate()
-								new_bullet.global_transform = muzzle.global_transform
-								var shotgunDirection = muzzle.global_transform.basis.z
+						
+						if weapon == 2:
+							
+							#shotgun bullet spawns
+							if ammo >= shotgunPellets:
+								for x in shotgunPellets:
+									var new_bullet : RigidBody3D = bullet_prefab.instantiate()
+									new_bullet.global_transform = muzzle.global_transform
+									var shotgunDirection = muzzle.global_transform.basis.z
 
-								shotgunDirection += muzzle.global_transform.basis.x * randf_range(-shotgunSpread, shotgunSpread)
-								shotgunDirection += muzzle.global_transform.basis.y * randf_range(-shotgunSpread, shotgunSpread)
-								shotgunDirection = shotgunDirection.normalized()
+									shotgunDirection += muzzle.global_transform.basis.x * randf_range(-shotgunSpread, shotgunSpread)
+									shotgunDirection += muzzle.global_transform.basis.y * randf_range(-shotgunSpread, shotgunSpread)
+									shotgunDirection = shotgunDirection.normalized()
 
-								new_bullet.apply_impulse(shotgunDirection * bullet_velocity)
-								
-								new_bullet.add_collision_exception_with($".")
-								new_bullet.add_collision_exception_with(new_bullet)
-								new_bullet.weaponDamage = bulletDamage
-								ammo += -1
-								get_parent().add_child(new_bullet)
+									new_bullet.apply_impulse(shotgunDirection * bullet_velocity)
+									
+									new_bullet.add_collision_exception_with($".")
+									new_bullet.add_collision_exception_with(new_bullet)
+									new_bullet.weaponDamage = bulletDamage
+									ammo += -1
+									get_parent().add_child(new_bullet)
+							else:
+								for x in ammo:
+									var new_bullet : RigidBody3D = bullet_prefab.instantiate()
+									new_bullet.global_transform = muzzle.global_transform
+									var shotgunDirection = muzzle.global_transform.basis.z
+
+									shotgunDirection += muzzle.global_transform.basis.x * randf_range(-shotgunSpread, shotgunSpread)
+									shotgunDirection += muzzle.global_transform.basis.y * randf_range(-shotgunSpread, shotgunSpread)
+									shotgunDirection = shotgunDirection.normalized()
+
+									new_bullet.apply_impulse(shotgunDirection * bullet_velocity)
+									
+									new_bullet.add_collision_exception_with($".")
+									new_bullet.add_collision_exception_with(new_bullet)
+									new_bullet.weaponDamage = bulletDamage
+									ammo += -1
+									get_parent().add_child(new_bullet)
+						
+						
+						# creating bullet for Washer
+						elif weapon == 3:
+							var new_bullet : RigidBody3D = bullet_prefab.instantiate()
+							new_bullet.global_transform = $Head/Camera3D/blockbench_export/Muzzle.global_transform
+							
+							# randomize direction
+							var washerDirection = muzzle.global_transform.basis.z
+							washerDirection += muzzle.global_transform.basis.x * randf_range(-washerSpread, washerSpread)
+							washerDirection += muzzle.global_transform.basis.y * randf_range(-washerSpread, washerSpread)
+							washerDirection = washerDirection.normalized()
+							
+							new_bullet.apply_impulse(washerDirection * bullet_velocity)
+							new_bullet.add_collision_exception_with($".")
+							new_bullet.add_collision_exception_with(new_bullet)
+							new_bullet.weaponDamage = bulletDamage
+							ammo += -1
+							get_parent().add_child(new_bullet)
+						
+						# other bullets
 						else:
-							for x in ammo:
-								var new_bullet : RigidBody3D = bullet_prefab.instantiate()
-								new_bullet.global_transform = muzzle.global_transform
-								var shotgunDirection = muzzle.global_transform.basis.z
-
-								shotgunDirection += muzzle.global_transform.basis.x * randf_range(-shotgunSpread, shotgunSpread)
-								shotgunDirection += muzzle.global_transform.basis.y * randf_range(-shotgunSpread, shotgunSpread)
-								shotgunDirection = shotgunDirection.normalized()
-
-								new_bullet.apply_impulse(shotgunDirection * bullet_velocity)
-								
-								new_bullet.add_collision_exception_with($".")
-								new_bullet.add_collision_exception_with(new_bullet)
-								new_bullet.weaponDamage = bulletDamage
-								ammo += -1
-								get_parent().add_child(new_bullet)
-					
-					
-					# creating bullet for Washer
-					elif weapon == 3:
-						var new_bullet : RigidBody3D = bullet_prefab.instantiate()
-						new_bullet.global_transform = $Head/Camera3D/blockbench_export/Muzzle.global_transform
+							var new_bullet : RigidBody3D = bullet_prefab.instantiate()
+							new_bullet.global_transform = $Head/Camera3D/blockbench_export/Muzzle.global_transform
+							new_bullet.apply_impulse($Head/Camera3D/blockbench_export/Muzzle.global_transform.basis.z * bullet_velocity)
+							new_bullet.add_collision_exception_with($".")
+							new_bullet.add_collision_exception_with(new_bullet)
+							new_bullet.weaponDamage = bulletDamage
+							ammo += -1
+							get_parent().add_child(new_bullet)
 						
-						# randomize direction
-						var washerDirection = muzzle.global_transform.basis.z
-						washerDirection += muzzle.global_transform.basis.x * randf_range(-washerSpread, washerSpread)
-						washerDirection += muzzle.global_transform.basis.y * randf_range(-washerSpread, washerSpread)
-						washerDirection = washerDirection.normalized()
 						
-						new_bullet.apply_impulse(washerDirection * bullet_velocity)
-						new_bullet.add_collision_exception_with($".")
-						new_bullet.add_collision_exception_with(new_bullet)
-						new_bullet.weaponDamage = bulletDamage
-						ammo += -1
-						get_parent().add_child(new_bullet)
-					
-					# other bullets
-					else:
-						var new_bullet : RigidBody3D = bullet_prefab.instantiate()
-						new_bullet.global_transform = $Head/Camera3D/blockbench_export/Muzzle.global_transform
-						new_bullet.apply_impulse($Head/Camera3D/blockbench_export/Muzzle.global_transform.basis.z * bullet_velocity)
-						new_bullet.add_collision_exception_with($".")
-						new_bullet.add_collision_exception_with(new_bullet)
-						new_bullet.weaponDamage = bulletDamage
-						ammo += -1
-						get_parent().add_child(new_bullet)
-					
-					
-					#starting firerate timer
-					can_shoot = false
-					shootTimer.start()
-					
-					# shoot sound
-					$SpraySound.play()
-					
-					if ammo == 0 and total_water != 0:
+						#starting firerate timer
+						can_shoot = false
+						shootTimer.start()
+						
+						# shoot sound
+						$SpraySound.play()
+						
+						if ammo == 0 and total_water != 0:
+							
+							reload()
+						
+					elif reloading == false and total_water != 0:
 						
 						reload()
-					
-				elif reloading == false and total_water != 0:
-					
-					reload()
-			
-	
-	#check for interaction collisions
-	check_hover_collision()
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		await_landing()
+				
+		
+		#check for interaction collisions
+		check_hover_collision()
+		
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+			await_landing()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		$FootstepSound.play()
-	
-	# Handle sprint
-	if Input.is_action_pressed("sprint") and reloading == false:
-		speed = SPRINT_SPEED
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			$FootstepSound.play()
 		
-	elif reloading == false:
-		speed = WALK_SPEED
-		if Input.is_action_just_released("sprint"):
-			$AnimationPlayer.play_backwards("sprint")
-		sprinting = false
-		$FootstepTimer.wait_time = walking_wait_time
-		
-	else:
-		speed = WALK_SPEED
-		sprinting = false
-		$FootstepTimer.wait_time = walking_wait_time
+		# Handle sprint
+		if Input.is_action_pressed("sprint") and reloading == false:
+			speed = SPRINT_SPEED
+			
+		elif reloading == false:
+			speed = WALK_SPEED
+			if Input.is_action_just_released("sprint"):
+				$AnimationPlayer.play_backwards("sprint")
+			sprinting = false
+			$FootstepTimer.wait_time = walking_wait_time
+			
+		else:
+			speed = WALK_SPEED
+			sprinting = false
+			$FootstepTimer.wait_time = walking_wait_time
 			
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-	
-	var input_dir := Input.get_vector("left", "right", "forward", "back")
-	var direction: Vector3 = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	if is_on_floor():
-		if direction:
-			moving = true
-			velocity.x = direction.x * speed
-			velocity.z = direction.z * speed
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		
+		
+		var input_dir := Input.get_vector("left", "right", "forward", "back")
+		var direction: Vector3 = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		
+		if is_on_floor():
+			if direction:
+				moving = true
+				velocity.x = direction.x * speed
+				velocity.z = direction.z * speed
+					
+			else:
+				moving = false
+				velocity.x = lerp(velocity.x, direction.x * speed, delta * 12.0)
+				velocity.z = lerp(velocity.z, direction.z * speed, delta * 12.0)
 				
 		else:
 			moving = false
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 12.0)
-			velocity.z = lerp(velocity.z, direction.z * speed, delta * 12.0)
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
 			
-	else:
-		moving = false
-		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
-		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+		# head bob
+		t_bob += delta * velocity.length() * float(is_on_floor())
+		camera.transform.origin = _headbob(t_bob)
+		gun.transform.origin = _headbob(-t_bob) - Vector3(-0.3, 0.7, 0.4)
 		
-	# head bob
-	t_bob += delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = _headbob(t_bob)
-	gun.transform.origin = _headbob(-t_bob) - Vector3(-0.3, 0.7, 0.4)
-	
-	# fov
-	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
-	var target_fov = fov + FOV_CHANGE * velocity_clamped
-	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
-	
-	move_and_slide()
-	
-	CoinLabel.text = str(playerCoins) + " Coins"
+		# fov
+		var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+		var target_fov = fov + FOV_CHANGE * velocity_clamped
+		camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+		
+		move_and_slide()
+		
+		CoinLabel.text = str(playerCoins) + " Coins"
 
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
